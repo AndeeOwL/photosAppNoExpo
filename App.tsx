79b5bf0cb6c3d19bcbcb121921b5cc2afa05aad2
andrean.buhchev <andeeowl@yahoo.com>
2022-12-16 14:51:25 +0200

    main

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 // BRANCH IBFix
+// BRNACH MAIN
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
